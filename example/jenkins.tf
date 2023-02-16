15edfbb9f0b9657e4170a850964ed2f41ecc3bd4
atishay.jain <atishay.jain@tothenew.com>
2023-02-16 18:46:28 +0530

    instance-profile added as default

--- example/jenkins.tf
+++ example/jenkins.tf
@@ -2,5 +2,4 @@ module "ec2-jenkins" {
   source               = "git::https://github.com/tothenew/terraform-aws-jenkins.git"
   vpc_id               = "vpc-99999999"
   subnet_ids           = ["subnet-99999999"]
-  iam_instance_profile = ""
 }
