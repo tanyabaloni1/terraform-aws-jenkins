cd57b483d668caa11d0ac056838af4ec60ec0714
tanyabaloni1 <85308025+tanyabaloni1@users.noreply.github.com>
2023-05-19 16:14:26 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-sudo yum install java-11-amazon-corretto -y
-sudo yum install unzip -y
-sudo yum install zip -y
-
+sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
-sudo systemctl start jenkins
 sudo systemctl enable jenkins
+sudo systemctl start jenkins
 sudo systemctl status jenkins
 jenkins --version
 
