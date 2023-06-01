087fc73b4e6a1904fe0d28f331491d3c0904d6c8
tanyabaloni1 <85308025+tanyabaloni1@users.noreply.github.com>
2023-06-01 17:45:55 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -34,24 +34,24 @@ cat > /etc/nginx/nginx.conf << EOF
 user nginx;
 worker_processes auto;
 error_log /var/log/nginx/error.log;
-pid /run/nginx.pid;
+#pid /run/nginx.pid;
 
-events {
-    worker_connections 1024;
-}
+#events {
+ #   worker_connections 1024;
+#}
 
 http {
     include /etc/nginx/mime.types;
     default_type application/octet-stream;
 
-    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                      '\$status \$body_bytes_sent "\$http_referer" '
-                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+#    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+#                     '\$status \$body_bytes_sent "\$http_referer" '
+#                      '"\$http_user_agent" "\$http_x_forwarded_for"';
 
     access_log  /var/log/nginx/access.log  main;
 
-    sendfile        on;
-    keepalive_timeout  65;
+   # sendfile        on;
+   # keepalive_timeout  65;
 
     server {
         listen 80;
@@ -59,8 +59,8 @@ http {
 
         location / {
             proxy_pass http://127.0.0.1:8080;
-            proxy_set_header Host \$host;
-            proxy_set_header X-Real-IP \$remote_addr;
+           # proxy_set_header Host \$host;
+           # proxy_set_header X-Real-IP \$remote_addr;
         }
     }
 }
